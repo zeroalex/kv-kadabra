fe3423a470d830f734c7b5b92d4b519ee568af8c
alexodisseus <alexodisseus@gmail.com>
2022-01-24 14:44:48 -0300

    first commit

--- main.py
+++ main.py
@@ -1,14 +1,39 @@
 import gi
 
+
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, GLib, Gdk
 
+
+
 from playkivy import *
+
+
+carregar = """#texto para inicio
+BoxLayout:
+   orientation:'vertical'
+   Label:
+      text:'texto textoi'
+   Button:
+      text:'botao'
+
+"""
+
+
 
 
 ui = Gtk.Builder()
 
 ui.add_from_file("main.glade")
+
+
+
+
+
+
+
+
+
 
 class Handler:
     def __init__(self, *args, **kwargs):
@@ -18,6 +43,10 @@ class Handler:
         self.entrada = ui.get_object("entrada")
         
         self.texto = ui.get_object("texto")
+
+        texto = self.texto.get_buffer()
+        texto.insert(texto.get_start_iter(),carregar)
+
         self.carregar = ""
 
 
@@ -41,29 +70,26 @@ class Handler:
         print(entrada)
 
     def recaregar(self, asd):
+        pass    
+    def visualizar(self,asd):
         texto = self.texto.get_buffer()
 
         print(texto.get_text(texto.get_start_iter(),texto.get_end_iter(),0 ))
         self.carregar = texto.get_text(texto.get_start_iter(),texto.get_end_iter(),0 )
     
-    def visualizar(self,asd):
-        print("kmsokmf")
-
+    
+        
         app = None
         app = PlaygroundApp(self.carregar)
         app.run()
 
-    
+
 
 ui.connect_signals(Handler())
 
-
 window = ui.get_object("ide")
+
 window.show_all()
 
+
 Gtk.main()
-
-
-
-
-
--- playkivy.py
+++ playkivy.py
@@ -1,25 +1,26 @@
 import kivy
 kivy.require('1.9.1')
 
+
 from kivy.app import App
-from kivy.core.clipboard import Clipboard
 from kivy.lang import Builder
 from kivy.lang import ParserException
-from kivy.core.window import Window
-
-
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.anchorlayout import AnchorLayout
-from kivy.uix.pagelayout import PageLayout
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.widget import Widget
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.textinput import TextInput
-
 from kivy.properties import ObjectProperty, StringProperty, BooleanProperty
-from kivy.storage.jsonstore import JsonStore
+ 
+from kivy.base import runTouchApp, async_runTouchApp, stopTouchApp
+
+"""
+
+#texto para inicio
+BoxLayout:
+   orientation:'vertical'
+   Label:
+      text:'texto textoi'
+   Button:
+      text:'botao'
+
+"""
 
 class PlaygroundScreen(Screen):
     content_pane = ObjectProperty(None)
@@ -29,34 +30,15 @@ class PlaygroundScreen(Screen):
     def __init__(self,entrada):
         self.entrada = entrada
         super(PlaygroundScreen,self).__init__()
-        text = "#type\nBoxLayout:\n   Label:\n      text:'hmlmi'\n   Button:\n      text:'hmlmi'"
-        print(self.entrada)
 
         self.creation = Builder.load_string(self.entrada)
         
         self.content_pane.add_widget(self.creation)
+
     
-    def on_text(self,val):
-        self.kivy_text = val
+    def start(self):
 
-        try:
-            creation = Builder.load_string(val)
-        except SyntaxError as se:
-            print("Not valid syntax.")
-            return
-        except ParserException as pe:
-            print("Parser error: {0}".format(pe))
-            return
-        except Exception as e:
-            print(type(e))
-            print(e)
-            return
-
-        try:
-            self.content_pane.clear_widgets()
-            self.content_pane.add_widget(creation)
-        except Exception as e:
-            print(e)
+        stopTouchApp()
 
 
 class PGScreenManager(ScreenManager):
